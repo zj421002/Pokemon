814f9bd5a6212ec71e7498513592e7cf2e30bb17
Zhang Jun <zj421002@hotmail.com>
2020-09-27 13:55:47 +0800

    Init
    Init the website

---
+++ pokemon.js
@@ -0,0 +1,9 @@
+let id;
+for(let i=1; i<=151;i++){
+    $("body").append('<img id="'+ i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+ i + '.png">');
+}
+
+$('img').click(function(){
+    id = $(this).attr('id');
+    parent.$(parent.document).trigger("imageClick",id);
+});
